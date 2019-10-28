a06af9c36643859603109ab3982d26abb2e7e29a
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-10-29 02:25:54 +0900

    change command namespace

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.insertPackageDecl', () => {
+	let disposable = vscode.commands.registerCommand('perl-insert-package.insertPackageDecl', () => {
 		// The code you place here will be executed every time your command is executed
 		const editor = vscode.window.activeTextEditor;
 		if (editor === undefined)
