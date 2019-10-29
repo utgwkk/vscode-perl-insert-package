a7c4c4c49c586915f6fe4a175f42db353eb76a52
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-10-30 01:02:49 +0900

    completion of package name from QuickPick

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,7 @@
+import * as path from 'path';
 import * as vscode from 'vscode';
 import { currentFileRelativePath } from './helper';
-import { makePackageDeclaration } from './path-to-package';
+import { makePackageDeclaration, makePackageName } from './path-to-package';
 
 export function insertPackageDecl(editor: vscode.TextEditor, editBuilder: vscode.TextEditorEdit) {
 	const relativePath = currentFileRelativePath(editor);
@@ -12,5 +13,33 @@ export function insertPackageDecl(editor: vscode.TextEditor, editBuilder: vscode
 	const packageDecl = makePackageDeclaration(relativePath);
 	editor.selections.forEach(selection => {
 		editBuilder.insert(selection.start, packageDecl);
+	});
+}
+
+export function insertPackageName() {
+	const files = vscode.workspace.findFiles('**/**.pm').then(urls => urls.map(url => {
+		const relativePath = vscode.workspace.asRelativePath(url);
+		return {
+			label: relativePath,
+			description: relativePath.replace(new RegExp(path.sep, 'g'), ' ').replace('.pm', ''),
+		};
+	}));
+	vscode.window.showQuickPick(files, { matchOnDescription: true }).then(selected => {
+		if (selected === undefined) {
+			return;
+		}
+		const packagePath = selected.label;
+		const editor = vscode.window.activeTextEditor;
+		if (editor === undefined) {
+			vscode.window.showErrorMessage('please open perl file');
+			return;
+		}
+
+		const packageName = makePackageName(packagePath);
+		editor.edit(editBuilder => {
+			editor.selections.forEach(selection => {
+				editBuilder.insert(selection.start, packageName);
+			});
+		});
 	});
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { insertPackageDecl } from './command';
+import { insertPackageDecl, insertPackageName } from './command';
 import { packageDeclCompletionProvider } from './completion';
 
 const perlSelector = { scheme: 'file', language: 'perl' };
@@ -12,6 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposables = [
 		vscode.languages.registerCompletionItemProvider(perlSelector, packageDeclCompletionProvider, 'packagedecl'),
 		vscode.commands.registerTextEditorCommand('perl-insert-package.insertPackageDecl', insertPackageDecl),
+		vscode.commands.registerCommand('perl-insert-package.insertPackageName', insertPackageName),
 	];
 
 	disposables.forEach(disposable => context.subscriptions.push(disposable));
