2d483378368d9f4024dccaf85d4b3ccc0a377040
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-10-29 14:56:37 +0900

    tab

--- src/command.ts
+++ src/command.ts
@@ -3,14 +3,14 @@ import { currentFileRelativePath } from './helper';
 import { makePackageDeclaration } from './path-to-package';
 
 export function insertPackageDecl(editor: vscode.TextEditor, editBuilder: vscode.TextEditorEdit) {
-    const relativePath = currentFileRelativePath(editor);
-    if (relativePath === undefined) {
-        vscode.window.showErrorMessage('please open perl file');
-        return;
-    }
+	const relativePath = currentFileRelativePath(editor);
+	if (relativePath === undefined) {
+		vscode.window.showErrorMessage('please open perl file');
+		return;
+	}
 
-    const packageDecl = makePackageDeclaration(relativePath);
-    editor.selections.forEach(selection => {
-        editBuilder.insert(selection.start, packageDecl);
-    });
+	const packageDecl = makePackageDeclaration(relativePath);
+	editor.selections.forEach(selection => {
+		editBuilder.insert(selection.start, packageDecl);
+	});
 }
