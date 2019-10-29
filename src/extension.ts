4c3d72c8e9030dd69023884d6be01b8f330c0011
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-10-29 13:30:39 +0900

    split into function

--- src/extension.ts
+++ src/extension.ts
@@ -14,24 +14,26 @@ function makePackageDeclaration(path: string): string {
 	return `package ${packageName};`;
 }
 
+function insertPackageDecl(editor: vscode.TextEditor, editBuilder: vscode.TextEditorEdit) {
+	const relativePath = currentFileRelativePath(editor);
+	if (relativePath === undefined) {
+		vscode.window.showErrorMessage('please open perl file');
+		return;
+	}
+
+	const packageDecl = makePackageDeclaration(relativePath);
+	editor.selections.forEach(selection => {
+		editBuilder.insert(selection.start, packageDecl);
+	});
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerTextEditorCommand('perl-insert-package.insertPackageDecl', (editor, editBuilder) => {
-		const relativePath = currentFileRelativePath(editor);
-		if (relativePath === undefined) {
-			vscode.window.showErrorMessage('please open perl file');
-			return;
-		}
-
-		const packageDecl = makePackageDeclaration(relativePath);
-		editor.selections.forEach(selection => {
-			editBuilder.insert(selection.start, packageDecl);
-		});
-	});
+	let disposable = vscode.commands.registerTextEditorCommand('perl-insert-package.insertPackageDecl', insertPackageDecl);
 
 	context.subscriptions.push(disposable);
 }
